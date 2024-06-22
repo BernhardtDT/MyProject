11f2df576bf06b282b4a3aa75eddd43908f4d58c
Stephen van der Merwe <stephenvdm123@gmail.com>
2024-06-22 21:32:42 +0200

    new age

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("Hello, Stephen!");
+Console.WriteLine("You are 19 years old");
 
 Console.WriteLine("New changes");
